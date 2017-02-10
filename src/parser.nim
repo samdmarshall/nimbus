58d0938f58319bc7a5873854fd33c1399f0165a1
Samantha Marshall <me@samdmarshall.com>
2017-02-09 19:10:56 -0500

    adding basic field parsing

--- src/parser.nim
+++ src/parser.nim
@@ -3,6 +3,7 @@
 # =======
 
 import os
+import strutils
 
 import libclang
 
@@ -19,20 +20,50 @@ proc visitChildrenCallback(cursor: CXCursor, parent: CXCursor, clientData: CXCli
   var file: CXFile
   libclang.getFileLocation(location, addr file, nil, nil, nil)
   let is_in_passed_header = ($libclang.getCString(libclang.getFileName(file)) == $file_path)
-  if is_in_passed_header == true:
+  if is_in_passed_header:
+    let name = $libclang.getCString(getCursorSpelling(cursor))
     case libclang.getCursorKind(cursor)
-    of CXCursorKind.StructDecl: discard
-    of CXCursorKind.UnionDecl: discard
-    of CXCursorKind.EnumDecl: discard
-    of CXCursorKind.FieldDecl: discard
-    of CXCursorKind.EnumConstantDecl: discard
-    of CXCursorKind.FunctionDecl: discard
+    of CXCursorKind.StructDecl:
+      echo("type " & name & "* = object")
+      discard visitChildren(cursor, visitChildrenCallback, clientData)
+    of CXCursorKind.UnionDecl:
+      echo("type " & name & "* {.union.} = object")
+      discard visitChildren(cursor, visitChildrenCallback, clientData)
+    of CXCursorKind.EnumDecl:
+      echo("type " & name & "* = enum")
+      discard visitChildren(cursor, visitChildrenCallback, clientData)
+    of CXCursorKind.FieldDecl:
+      echo("  " & name)
+      discard
+    of CXCursorKind.EnumConstantDecl:
+      echo("  " & name & ",")
+      discard
+    of CXCursorKind.FunctionDecl:
+      echo("proc " & name & "* = ")
+      discard visitChildren(cursor, visitChildrenCallback, clientData)
     of CXCursorKind.ObjCInterfaceDecl:
-      echo($libclang.getCString(getCursorSpelling(cursor)))
-    of CXCursorKind.ObjCCategoryDecl: discard
-    of CXCursorKind.ObjCProtocolDecl: discard
+      echo("type " & name & "* = Id")
+      discard visitChildren(cursor, visitChildrenCallback, clientData)
+    of CXCursorKind.ObjCCategoryDecl:
+      discard visitChildren(cursor, visitChildrenCallback, clientData)
+    of CXCursorKind.ObjCProtocolDecl:
+      discard visitChildren(cursor, visitChildrenCallback, clientData)
+    of CXCursorKind.ObjCInstanceMethodDecl:
+      let argument_count = strutils.count(name, ":")
+      let replaced_method_string = strutils.replace(name, ":", "_")
+      let method_string = if strutils.endsWith(replaced_method_string, "_"): replaced_method_string[0 .. replaced_method_string.len - 2]
+                          else: replaced_method_string
+      #echo("proc " & method_string & "*(self: object")
+      if argument_count > 0:
+        for index in 0..argument_count:
+          discard visitChildren(cursor, visitChildrenCallback, clientData)
+      #echo(") {.importobjc: \"" & method_string & "\", noDecl.}")
+    of CXCursorKind.ObjCClassMethodDecl: discard
+    of CXCursorKind.TypedefDecl: discard
+    of CXCursorKind.ParmDecl:
+      #echo(name)
+      discard
     else: discard
-  discard visitChildren(cursor, visitChildrenCallback, clientData)
   result = CXChildVisitResult.Continue
 
 # ==========
@@ -40,6 +71,11 @@ proc visitChildrenCallback(cursor: CXCursor, parent: CXCursor, clientData: CXCli
 # ==========
 
 proc parseTranslationUnit*(file_path: string, input_language: Language): void =
+  case input_language:
+  of Language.ObjC:
+    echo("type Id {.importc: \"id\", header: \"<objc/NSObject.h>\", final.} = distinct int")
+  else:
+    discard
   let arguments: seq[string] = @["-x", $input_language, "-I/usr/include/", "-I."]
   let args_cstring = allocCStringArray(arguments)
   let index = libclang.createIndex(1,1)
